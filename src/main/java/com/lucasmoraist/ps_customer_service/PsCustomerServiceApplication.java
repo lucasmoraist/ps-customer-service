674b548123d95dae32f3313882aeb6f18268f053
lucasmoraist <luksmnt1101@gmail.com>
2025-12-27 00:39:59 -0300

    first commit

---
+++ src/main/java/com/lucasmoraist/ps_customer_service/PsCustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lucasmoraist.ps_customer_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsCustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsCustomerServiceApplication.class, args);
+	}
+
+}
