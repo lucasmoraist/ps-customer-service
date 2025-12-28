bdaebac708a01dbc561f22e662a3e424ae480188
lucasmoraist <luksmnt1101@gmail.com>
2025-12-27 23:04:36 -0300

    feat: add listCustomers method to CustomerController and CustomerRoutes

--- src/main/java/com/lucasmoraist/ps_customer_service/infrastructure/api/web/controller/CustomerController.java
+++ src/main/java/com/lucasmoraist/ps_customer_service/infrastructure/api/web/controller/CustomerController.java
@@ -3,23 +3,34 @@ package com.lucasmoraist.ps_customer_service.infrastructure.api.web.controller;
 import com.lucasmoraist.ps_customer_service.application.dto.CustomerDTO;
 import com.lucasmoraist.ps_customer_service.application.mapper.CustomerMapper;
 import com.lucasmoraist.ps_customer_service.application.usecases.customer.CreateCustomerCase;
+import com.lucasmoraist.ps_customer_service.application.usecases.customer.ListCustomersCase;
+import com.lucasmoraist.ps_customer_service.domain.model.Customer;
 import com.lucasmoraist.ps_customer_service.infrastructure.api.web.request.CustomerRequest;
 import com.lucasmoraist.ps_customer_service.infrastructure.api.web.router.CustomerRoutes;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 public class CustomerController implements CustomerRoutes {
 
     private final CreateCustomerCase createCustomerCase;
+    private final ListCustomersCase listCustomersCase;
 
     @Override
     public ResponseEntity<Void> registerCustomer(CustomerRequest request) {
         CustomerDTO dto = CustomerMapper.toDto(request);
         this.createCustomerCase.execute(dto);
         return ResponseEntity.status(201).build();
+    }
+
+    @Override
+    public ResponseEntity<List<Customer>> listCustomers() {
+        List<Customer> customers = this.listCustomersCase.execute();
+        return ResponseEntity.ok(customers);
     }
 
 }
--- src/main/java/com/lucasmoraist/ps_customer_service/infrastructure/api/web/router/CustomerRoutes.java
+++ src/main/java/com/lucasmoraist/ps_customer_service/infrastructure/api/web/router/CustomerRoutes.java
@@ -1,16 +1,23 @@
 package com.lucasmoraist.ps_customer_service.infrastructure.api.web.router;
 
+import com.lucasmoraist.ps_customer_service.domain.model.Customer;
 import com.lucasmoraist.ps_customer_service.infrastructure.api.web.request.CustomerRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequestMapping("/api/v1/customer")
 public interface CustomerRoutes {
 
     @PostMapping("/register")
     ResponseEntity<Void> registerCustomer(@RequestBody @Valid CustomerRequest request);
+
+    @GetMapping("/list")
+    ResponseEntity<List<Customer>> listCustomers();
 
 }
