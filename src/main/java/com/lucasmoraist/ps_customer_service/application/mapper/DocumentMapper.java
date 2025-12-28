f2075e3fabf38c32dc93b0d9e4f60ea5b403152c
lucasmoraist <luksmnt1101@gmail.com>
2025-12-27 22:52:49 -0300

    feat: add mapping methods to convert DocumentRequest and PaymentKeyRequest to their respective DTOs

--- src/main/java/com/lucasmoraist/ps_customer_service/application/mapper/DocumentMapper.java
+++ src/main/java/com/lucasmoraist/ps_customer_service/application/mapper/DocumentMapper.java
@@ -1,12 +1,23 @@
 package com.lucasmoraist.ps_customer_service.application.mapper;
 
+import com.lucasmoraist.ps_customer_service.domain.enums.DocumentType;
 import com.lucasmoraist.ps_customer_service.domain.model.Document;
+import com.lucasmoraist.ps_customer_service.infrastructure.api.web.request.DocumentRequest;
 import com.lucasmoraist.ps_customer_service.infrastructure.database.entity.DocumentEntity;
 
 public final class DocumentMapper {
 
     private DocumentMapper() {
         throw new IllegalStateException("Utility class");
+    }
+
+    public static Document toDto(DocumentRequest documentRequest) {
+        return new Document(
+                null,
+                DocumentType.fromValue(documentRequest.number()),
+                documentRequest.number(),
+                null
+        );
     }
 
     public static DocumentEntity toEntity(Document document) {
--- src/main/java/com/lucasmoraist/ps_customer_service/application/mapper/PaymentKeyMapper.java
+++ src/main/java/com/lucasmoraist/ps_customer_service/application/mapper/PaymentKeyMapper.java
@@ -1,12 +1,21 @@
 package com.lucasmoraist.ps_customer_service.application.mapper;
 
 import com.lucasmoraist.ps_customer_service.domain.model.PaymentKey;
+import com.lucasmoraist.ps_customer_service.infrastructure.api.web.request.PaymentKeyRequest;
 import com.lucasmoraist.ps_customer_service.infrastructure.database.entity.PaymentKeyEntity;
 
 public final class PaymentKeyMapper {
 
     private PaymentKeyMapper() {
         throw new IllegalStateException("Utility class");
+    }
+
+    public static PaymentKey toDto(PaymentKeyRequest paymentKeyRequest) {
+        return new PaymentKey(
+                null,
+                paymentKeyRequest.keyValue(),
+                null
+        );
     }
 
     public static PaymentKeyEntity toEntity(PaymentKey paymentKey) {
