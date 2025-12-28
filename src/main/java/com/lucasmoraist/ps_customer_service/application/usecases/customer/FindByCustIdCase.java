8af9a4f790e977b8e69c0ea0f99aa7ec1f7faff3
lucasmoraist <luksmnt1101@gmail.com>
2025-12-28 10:55:28 -0300

    feat: add method to find customer by id

--- src/main/java/com/lucasmoraist/ps_customer_service/application/usecases/customer/FindByCustIdCase.java
+++ src/main/java/com/lucasmoraist/ps_customer_service/application/usecases/customer/FindByCustIdCase.java
@@ -3,18 +3,18 @@ package com.lucasmoraist.ps_customer_service.application.usecases.customer;
 import com.lucasmoraist.ps_customer_service.application.gateway.CustomerPersistence;
 import com.lucasmoraist.ps_customer_service.domain.model.Customer;
 
-import java.util.List;
+import java.util.UUID;
 
-public class ListCustomersCase {
+public class FindByCustIdCase {
 
     private final CustomerPersistence customerPersistence;
 
-    public ListCustomersCase(CustomerPersistence customerPersistence) {
+    public FindByCustIdCase(CustomerPersistence customerPersistence) {
         this.customerPersistence = customerPersistence;
     }
 
-    public List<Customer> execute() {
-        return customerPersistence.findAll();
+    public Customer execute(UUID id) {
+        return this.customerPersistence.findById(id);
     }
 
 }
