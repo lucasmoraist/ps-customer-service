7035c3b51208026250a4b756cd1ee054239a1a7c
lucasmoraist <luksmnt1101@gmail.com>
2025-12-27 22:21:10 -0300

    feat: add CustomerRepository interface for database access

---
+++ src/main/java/com/lucasmoraist/ps_customer_service/infrastructure/database/repository/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.lucasmoraist.ps_customer_service.infrastructure.database.repository;
+
+import com.lucasmoraist.ps_customer_service.infrastructure.database.entity.CustomerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<CustomerEntity, UUID> {
+
+}
