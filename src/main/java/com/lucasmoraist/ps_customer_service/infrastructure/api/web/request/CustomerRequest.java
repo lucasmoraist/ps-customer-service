1a34eec96eb06fb66a08dac1ae48d4caef0e5301
lucasmoraist <luksmnt1101@gmail.com>
2025-12-27 22:33:53 -0300

    feat: add CustomerRequest record for customer input validation

---
+++ src/main/java/com/lucasmoraist/ps_customer_service/infrastructure/api/web/request/CustomerRequest.java
@@ -0,0 +1,26 @@
+package com.lucasmoraist.ps_customer_service.infrastructure.api.web.request;
+
+import com.lucasmoraist.ps_customer_service.domain.model.Document;
+import com.lucasmoraist.ps_customer_service.domain.model.PaymentKey;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+
+public record CustomerRequest(
+        @NotBlank(message = "Name is required")
+        @Size(min = 3, message = "Name must be at least 3 characters long")
+        String name,
+        @NotBlank(message = "Email is required")
+        @Email(message = "Invalid email format")
+        String email,
+        @Size(min = 6, message = "Password must be at least 6 characters long")
+        String password,
+        @NotEmpty(message = "At least one document is required")
+        List<Document> documents,
+        List<PaymentKey> paymentKeys
+) {
+
+}
