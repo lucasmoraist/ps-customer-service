e683e375cc2d3cd4bac374d57c823a2b12600463
lucasmoraist <luksmnt1101@gmail.com>
2025-12-27 23:22:46 -0300

    feat: add findByEmail method to CustomerRepository for retrieving customer by email

--- src/main/java/com/lucasmoraist/ps_customer_service/infrastructure/database/repository/CustomerRepository.java
+++ src/main/java/com/lucasmoraist/ps_customer_service/infrastructure/database/repository/CustomerRepository.java
@@ -4,9 +4,10 @@ import com.lucasmoraist.ps_customer_service.infrastructure.database.entity.Custo
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<CustomerEntity, UUID> {
-
+    Optional<CustomerEntity> findByEmail(String email);
 }
